5e32322b7746feb64918e0842530968ff7a803e3
Kaseres <arenas.2533@gmail.com>
2022-03-21 03:19:59 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="bg-red-500 text-xl flex justify-center items-center min-h-screen text-white font-bold">
+        Hello World
+      <button class="flex items-center justify-center h-10 w-40 bg-blue-500 round-full text-lg text-white hover:bg-blue-400 ml-10 mr-10">
+        Click
+      </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
